26ec706bdf6f8b5a961854377718f75cf34a0307
Michalina Dengusiak <michalina.dengusiak@gmail.com>
2026-04-28 08:44:31 +0200

    Added Github Pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   resolve: {
     alias: { '@': path.resolve(__dirname, './src') },
   },
+  base:'bee_haviour/',
   server: { port: 3000, open: true },
   test: {
     globals: true,
